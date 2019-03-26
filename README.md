539ffe351a232c781016009b448bc61a798dfa4a
Mutmut <briend.mathilde@gmail.com>
2019-03-26 16:06:47 +0100

    Popup qui fonctionne mais pourrait mieux faire

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # THP-JS
+
+Ceci est ma version de Better Motherfucking Website avec une popup JavaScript :)
+
+Lien vers la version en prod : https://mutmutblop.github.io/THP-JS/
---
+++ main.js
@@ -0,0 +1,5 @@
+setTimeout(function(){
+	if (confirm ("Hey le site xxxdivsxxx est trop bien. Viens dessus stp please")) {
+		window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+	};
+}, 10000);
